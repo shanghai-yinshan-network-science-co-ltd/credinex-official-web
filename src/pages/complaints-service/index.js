2faf8c0ffd5506dae0ae17651a678ad499d1ee75
cleverdou <lostaloneesk@126.com>
2020-12-03 15:41:57 +0800

    feat:ojk合规修改

--- src/pages/complaints-service/index.js
+++ src/pages/complaints-service/index.js
@@ -1,70 +1,67 @@
 import styles from './index.scss';
 
 export default function(props) {
-
   return (
     <div className={styles.contactus}>
       <div className={styles.banner}>
         <div className={styles.msg}>
           <h5>Layanan Pengaduan Pelanggan CrediNex</h5>
-          <p>Layanan Pengaduan CrediNex ini dibentuk dan disusun berdasarkan
-            Peraturan Otoritas Jasa Keuangan No.18/POJK.07/2018 tentang Layanan
-            Pengaduan Konsumen di Sektor Jasa Keuangan dan Surat Edaran Otoritas
-            Jasa Keuangan No. 2/SEOJK.07/2014 tentang Pelayanan dan Penyelesaian
-            Pengaduan Konsumen pada Pelaku Usaha Jasa Keuangan.</p>
-          <p>CrediNex berkomitmen untuk menjalin hubungan baik dengan Pengguna
-            layanan Kami dan juga memberikan layanan pengaduan terbaik dengan
-            menerapkan prinsip transparansi dan terbuka dalam memberikan
-            informasi kepada Konsumen.</p>
+          <p>
+            Layanan Pengaduan CrediNex ini dibentuk dan disusun berdasarkan Peraturan Otoritas Jasa
+            Keuangan No.18/POJK.07/2018 tentang Layanan Pengaduan Konsumen di Sektor Jasa Keuangan
+            dan Surat Edaran Otoritas Jasa Keuangan No. 2/SEOJK.07/2014 tentang Pelayanan dan
+            Penyelesaian Pengaduan Konsumen pada Pelaku Usaha Jasa Keuangan.
+          </p>
+          <p>
+            CrediNex berkomitmen untuk menjalin hubungan baik dengan Pengguna layanan Kami dan juga
+            memberikan layanan pengaduan terbaik dengan menerapkan prinsip transparansi dan terbuka
+            dalam memberikan informasi kepada Konsumen.
+          </p>
 
           <h6>1.Penerima Pengaduan</h6>
-          <p>Setiap Konsumen CrediNex dapat menyampaikan pengaduan melalui
-            beberapa sarana yang telah kami sediakan baik secara lisan seperti
-            telepon ke hotline kami di (021) – 39506666 atau mendatangi langsung
-            kantor operasional CrediNex yang tertera pada halaman Website.
-            Selain itu pengguna CrediNex juga dapat menyampaikan pengaduannya
-            secara lisan melalui Live Chat Aplikasi, E-mail ke
-            csofficial@credinex.id, maupun social media Kami yaitu Facebook:
-            CrediNex, Instagram: CrediNex.Id, Twitter: @CrediNex</p>
+          <p>
+            Setiap Konsumen CrediNex dapat menyampaikan pengaduan melalui beberapa sarana yang telah
+            kami sediakan baik secara lisan seperti telepon ke hotline kami di (021) – 39506666 atau
+            mendatangi langsung kantor operasional CrediNex yang tertera pada halaman Website.
+            Selain itu pengguna CrediNex juga dapat menyampaikan pengaduannya secara lisan melalui
+            Live Chat Aplikasi, E-mail ke csofficial@credinex.id, maupun social media Kami yaitu
+            Facebook: CrediNex, Instagram: CrediNex.Id, Twitter: @CrediNex
+          </p>
 
           <h6>2.Penanganan Pengaduan</h6>
-          <p>Setelah pengaduan Konsumen Kami terima, tim Customer Service Kami
-            akan memberikan respon dan nomor laporan aduan (jika melibatkan
-            divisi internal lainnya) yaitu:<br /><br />
-
-            Hari Kerja : Senin s/d Jum’at pukul 08.00 WIB hingga 20.00 WIB<br />
-            Hari Libur : Sabtu dan Minggu pukul 08.00 WIB hingga 17.00 WIB<br /><br />
-
-            Tim Customer Service Kami akan memberikan respon selambat-lambatnya
-            5 (lima) Hari Kerja setelah aduan diterima.</p>
+          <p>
+            Setelah pengaduan Konsumen Kami terima, tim Customer Service Kami akan memberikan respon
+            dan nomor laporan aduan (jika melibatkan divisi internal lainnya) yaitu:
+            <br />
+            <br />
+            Hari Kerja : Senin s/d Jum’at pukul 08.00 WIB hingga 20.00 WIB
+            <br />
+            Hari Libur : Sabtu dan Minggu pukul 08.00 WIB hingga 17.00 WIB
+            <br />
+            <br />
+            Tim Customer Service Kami akan memberikan respon selambat-lambatnya 5 (lima) Hari Kerja
+            setelah aduan diterima.
+          </p>
 
           <h6>3.Penyelesaian Pengaduan</h6>
           <p>
-            Tim Customer Service CrediNex dapat secara langsung melakukan
-            penyelesaian terhadap aduan Konsumen apabila data dan informasi yang
-            diterima telah akurat dan benar. Terhadap aduan tersebut dapat
-            diberikan solusi secara langsung tanpa melibatkan divisi internal
-            lainnya sejak pengaduan diterima. Jika pengaduan melibatkan divisi
-            internal lainnya atau pihak ketiga, CrediNex membutuhkan waktu
-            penyelesaian pengaduan Konsumen selambat-lambatnya dalam jangka
-            waktu 20 (dua puluh) Hari Kerja atau lebih tergantung pada tingkat
-            kompleksitas pengaduan Konsumen.
+            Tim Customer Service CrediNex dapat secara langsung melakukan penyelesaian terhadap
+            aduan Konsumen apabila data dan informasi yang diterima telah akurat dan benar. Terhadap
+            aduan tersebut dapat diberikan solusi secara langsung tanpa melibatkan divisi internal
+            lainnya sejak pengaduan diterima. Jika pengaduan melibatkan divisi internal lainnya atau
+            pihak ketiga, CrediNex membutuhkan waktu penyelesaian pengaduan Konsumen
+            selambat-lambatnya dalam jangka waktu 20 (dua puluh) Hari Kerja atau lebih tergantung
+            pada tingkat kompleksitas pengaduan Konsumen.
           </p>
           <h6>4.Pelaporan Pengaduan ke OJK</h6>
           <p>
-            CrediNex secara berkala melakukan Pelaporan Pengaduan Konsumen ke
-            Otoritas Jasa Keuangan sebagai bentuk pertanggungjawaban penyedia
-            layanan pengaduan konsumen disertai dengan status pengaduan, jenis
-            pengaduan dan penyebab pengaduan untuk periode bulanan dan triwulan
-            yang formatnya sudah ditentukan oleh Otoritas Jasa Keuangan.
+            CrediNex secara berkala melakukan Pelaporan Pengaduan Konsumen ke Otoritas Jasa Keuangan
+            sebagai bentuk pertanggungjawaban penyedia layanan pengaduan konsumen disertai dengan
+            status pengaduan, jenis pengaduan dan penyebab pengaduan untuk periode bulanan dan
+            triwulan yang formatnya sudah ditentukan oleh Otoritas Jasa Keuangan.
           </p>
         </div>
       </div>
     </div>
   );
 }
-
-
-
-
-
